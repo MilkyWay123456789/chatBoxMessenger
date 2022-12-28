a0cca0d40fbc14cac5e8220d806b89a6ce18b99b
htcreater <tienph.19th@sv.dla.edu.vn>
2022-12-28 08:29:02 +0700

    up

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,7 +6,7 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
 
-    router.get("/setup-profile", homeController.setupProfile)
+    router.post("/setup-profile", homeController.setupProfile)
 
     router.post('/webhook', homeController.postWebhook);
     router.get('/webhook', homeController.getWebhook)
